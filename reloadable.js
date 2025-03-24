8c8f29361ff6dccf782f5cb3f1218be6d591d1ed
allnulled <todosanulados@gmail.com>
2025-03-24 18:50:19 +0100

    agenda should work

--- reloadable.js
+++ reloadable.js
@@ -1,7 +1,10 @@
 const serverUrl = 'http://127.0.0.1';
 const serverPort = 3000;
-const socket = io(`${serverUrl}:${serverPort}`);
-socket.on('refrescar', () => {
-  console.log('Recibida la señal de refrescar desde el servidor');
-  location.reload();
-});
+
+if (process?.env?.NODE_ENV === "test") {
+  const socket = io(`${serverUrl}:${serverPort}`);
+  socket.on('refrescar', () => {
+    console.log('Recibida la señal de refrescar desde el servidor');
+    location.reload();
+  });
+}
